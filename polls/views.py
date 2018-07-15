53fbe5fb498daad75e237bdcd230a5a90245ce64
CanJam <982139083@qq.com>
2018-07-16 02:26:08 +0800

    detail页

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 from .models import Question
 """
@@ -35,9 +36,13 @@ def index(request):
 def detail(request, question_id): #detail(request=<HttpRequest object>, question_id=34)调用形式
     return HttpResponse("You're looking at question %s." % question_id)
 #优化1：引入404异常，如果指定问题 ID 所对应的问题不存在，这个视图就会抛出一个 Http404 异常
-#def detail(request,question_id):
-  #  try:
-    #    question = Question.objects.get(pk=question_id)
+#使用（try: except raise） 一般抛异常都是用try
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'polls/detail.html',{'question':question})        
 def results(request, question_id):    
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
