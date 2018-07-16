914af6dc836b2130aa0efffb9b535088f136b0c2
CanJam <982139083@qq.com>
2018-07-16 15:10:04 +0800

    未使用通用视图前

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path
 from .import views
 #函数 path() 具有四个参数，两个必须参数：route 和 view，两个可选参数：kwargs 和 name
+app_name = 'polls' #为 URL 名称添加命名空间
 urlpatterns = [
        path('',views.index,name='index'),#path(‘’（链接），方法名，name=‘’)，path的格式
        # path('nono/',views.nono,name='i'),#自己测试语句，
-       path('<int:question_id>/',views.detail,name='detail'),
+       path('<int:question_id>/', views.detail, name='detail'),
        path('<int:question_id>/results/',views.results,name='results'),#结果页
        path('<int:question_id>/vote/',views.vote,name='vote'), #投票页
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 #！-*-coding：UTF-8-*-
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question,Choice
 """
 #问题：页面的设计写死在视图函数的代码里的
 def index(request): #定义一个方法请求
@@ -32,7 +33,8 @@ def index(request):
     }  
     return render(request,'polls/index.html',context)
 # def nono(request): 自定义
- #   return HttpResponse('nonoo')  
+ # return HttpResponse('nonoo')  
+"""
 def detail(request, question_id): #detail(request=<HttpRequest object>, question_id=34)调用形式
     return HttpResponse("You're looking at question %s." % question_id)
 #优化1：引入404异常，如果指定问题 ID 所对应的问题不存在，这个视图就会抛出一个 Http404 异常
@@ -42,12 +44,44 @@ def detail(request,question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request,'polls/detail.html',{'question':question})        
+    return render(request,'polls/detail.html',{'question':question})  
+"""
+#优化2：引用get_object_or_404()
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/detail.html',{'question':question})    
+"""
 def results(request, question_id):    
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
-
+#优化1：当有人对 Question 进行投票后， vote() 视图将请求重定向到 Question 的结果界面
+"""
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})    
+"""
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)    
+优化1：引入404异常,
+request.POST 是一个类字典对象，通过关键字的名字获取提交的数据。 
+request.POST['choice'] 以字符串形式返回选择的 Choice 的 ID,值永远是字符串。
+如果在 request.POST['choice'] 数据中没有提供 choice ， POST 将引发一个 KeyError 。
+代码检查 KeyError ，如果没有给出 choice 将重新显示 Question 表单和一个错误信息
+在 HttpResponseRedirect 的构造函数中使用 reverse() 函数。这个函数避免了我们在视图函数中硬编码 URL。
+它需要我们给出我们想要跳转的视图的名字和该视图所对应的 URL 模式中需要给该视图提供的参数
+"""
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message':"You didn't select a choice.",
+            })    
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))        
 # Create your views here.
 
