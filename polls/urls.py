6304169b75c16e80e17f1e42830b2a5070425ca0
CanJam <982139083@qq.com>
2018-07-16 15:31:16 +0800

    加入通用视图.

--- polls/urls.py
+++ polls/urls.py
@@ -3,10 +3,19 @@ from django.urls import path
 from .import views
 #函数 path() 具有四个参数，两个必须参数：route 和 view，两个可选参数：kwargs 和 name
 app_name = 'polls' #为 URL 名称添加命名空间
+"""
 urlpatterns = [
        path('',views.index,name='index'),#path(‘’（链接），方法名，name=‘’)，path的格式
        # path('nono/',views.nono,name='i'),#自己测试语句，
        path('<int:question_id>/', views.detail, name='detail'),
        path('<int:question_id>/results/',views.results,name='results'),#结果页
        path('<int:question_id>/vote/',views.vote,name='vote'), #投票页
+]
+"""
+#优化1：导入通用试图
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question,Choice
 """
@@ -14,6 +15,7 @@ def index(request): #定义一个方法请求
     return HttpResponse(output)
 
 #优化1：在templates 目录建立独立的 index.html将页面的设计从代码中分离出来
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5] 
     template = loader.get_template('polls/index.html')
@@ -21,11 +23,12 @@ def index(request):
         'latest_question_list':latest_question_list,
     }   
     return HttpResponse(template.render(context,request))
-"""    
+ 
 #使用快捷函数render()(请求对象,模板名,字典作为可选)函数将请求对象的参数返回给定的上下文所呈现的给定模板的HttpResponse对象
 #The render() function takes the request object as its first argument, a template name as its second argument and a dictionary as its optional third argument.
 #It returns an HttpResponse object of the given template rendered with the given context.
 #优化2：使用render()方法   
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
@@ -35,37 +38,66 @@ def index(request):
 # def nono(request): 自定义
  # return HttpResponse('nonoo')  
 """
+#优化3,导入通用视图
+#ListView，自动生成的 context 变量是 question_list。
+#为了覆盖这个行为，我们提供 context_object_name 属性，表示我们想使用 latest_question_list。
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+"""
 def detail(request, question_id): #detail(request=<HttpRequest object>, question_id=34)调用形式
     return HttpResponse("You're looking at question %s." % question_id)
+
 #优化1：引入404异常，如果指定问题 ID 所对应的问题不存在，这个视图就会抛出一个 Http404 异常
 #使用（try: except raise） 一般抛异常都是用try
+
 def detail(request,question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request,'polls/detail.html',{'question':question})  
-"""
+
 #优化2：引用get_object_or_404()
 def detail(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html',{'question':question})    
 """
+#优化3,导入通用视图
+#DetailView 期望从 URL 中捕获名为 "pk" 的主键值，所以我们为通用视图把 question_id 改成 pk,可见"polls/urls.py"
+ #template_name 属性是用来告诉 Django 使用一个指定的模板名字，而不是自动生成的默认名字
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+"""    
 def results(request, question_id):    
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 #优化1：当有人对 Question 进行投票后， vote() 视图将请求重定向到 Question 的结果界面
-"""
+
 def results(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/results.html',{'question':question})    
 """
+#优化3,导入通用视图
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+"""    
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)    
+
 优化1：引入404异常,
 request.POST 是一个类字典对象，通过关键字的名字获取提交的数据。 
 request.POST['choice'] 以字符串形式返回选择的 Choice 的 ID,值永远是字符串。
-如果在 request.POST['choice'] 数据中没有提供 choice ， POST 将引发一个 KeyError 。
+如果在 request.POST['choicfrom django.views import generice'] 数据中没有提供 choice ， POST 将引发一个 KeyError 。
 代码检查 KeyError ，如果没有给出 choice 将重新显示 Question 表单和一个错误信息
 在 HttpResponseRedirect 的构造函数中使用 reverse() 函数。这个函数避免了我们在视图函数中硬编码 URL。
 它需要我们给出我们想要跳转的视图的名字和该视图所对应的 URL 模式中需要给该视图提供的参数
